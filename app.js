ad4e945e323a2b5b74f94e286397945417f91974
Joseph Heidari <agile.8272@gmail.com>
2019-05-14 17:17:37 +0430

    Add reset, start and stop buttons
    Now all reset, start and stop buttons are working

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var startButton = document.querySelector("#js--start");
 var stopButton = document.querySelector("#js--stop");
+var resetButton = document.querySelector("#js--reset");
 var stopwatch = document.querySelector("#js--stopwatch");
+
+// Show an element on screen
+function hide(element) {
+  element.classList.add("display-none");
+}
+
+// Hide an element on screen
+function show(element) {
+  element.classList.remove("display-none");
+}
 
 var mil = 0;
 var displayMil = "00";
@@ -8,6 +19,9 @@ var seconds = 0;
 var displaySeconds = "00";
 
 startButton.addEventListener("click", function() {
+  hide(startButton);
+  hide(resetButton);
+  show(stopButton);
   var timer = setInterval(function() {
     mil = mil + 1;
 
@@ -32,6 +46,20 @@ startButton.addEventListener("click", function() {
   }, 10);
 
   stopButton.addEventListener("click", function() {
+    hide(stopButton);
+    show(startButton);
+    show(resetButton);
     clearInterval(timer);
   });
+});
+
+resetButton.addEventListener("click", function() {
+  hide(resetButton);
+  hide(stopButton);
+
+  mil = 0;
+  seconds = 0;
+  displayMil = "00";
+  displaySeconds = "00";
+  stopwatch.innerHTML = displaySeconds + "." + displayMil;
 });
